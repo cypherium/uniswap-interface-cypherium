6f1e376aac346f1e31b7e10bad28e4dd669b96d3
japson1987 <276286171@qq.com>
2022-06-29 15:10:28 +0800

    First commit.

---
+++ src/constants/multicall/index.ts
@@ -0,0 +1,13 @@
+import { ChainId } from 'cypheriumswap'
+import MULTICALL_ABI from './abi.json'
+import { multicall } from '../../moonbase_address.json'
+
+const MULTICALL_NETWORKS: { [chainId in ChainId]: string } = {
+  [ChainId.CYPHER_MAINNET]: '0xeefBa1e63905eF1D7ACbA5a8513c70307C1cE441',
+  [ChainId.CYPHER_ROPSTEN]: '0xB4656db1dcE1A268139A7A28Ac288526689cD696',
+  [ChainId.CYPHER_RINKEBY]: multicall,
+  [ChainId.CYPHER_GÖRLI]: multicall,
+  [ChainId.CYPHER_KOVAN]: multicall
+}
+
+export { MULTICALL_ABI, MULTICALL_NETWORKS }
